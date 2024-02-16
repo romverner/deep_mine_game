b88de6863e1db1da5d99e09aece485075de5ec17
Roman Verner <romanv@romans-mbp.lan>
2024-02-16 11:32:49 -0500

    reworked stone tile phsyics

--- terrain/collapsible_block/collapsible_block.gd
+++ terrain/collapsible_block/collapsible_block.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends StaticBody2D
 
 class_name colapsible_block
 
@@ -9,33 +9,39 @@ signal falling
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var _starting_pos = null
+var _last_known_pos = null
 var _is_falling = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_starting_pos = global_position
-	#freeze = true
-
-func _integrate_forces(state):
-	var velocity = state.linear_velocity
 	
-	if snappedf(velocity.y, 1.0) != 0:
-		texture_sprite.visible = false
-	else:
-		texture_sprite.visible = true
+func _snap_to_grid():
+	var current_y = int(snappedf(position.y, 1.0))
+	var new_y = current_y - (current_y % 16) + 8
+	position.y = new_y
+	
+	# Saving last position for nebulous save-game feature at some point
+	_last_known_pos = global_position
 
-func _on_impact_area_body_exited(body):
-	if !(body is Player):
-		freeze = false
-		falling.emit()
+# Using built-in phsyics caused lag when too many stones on screen because every frame the
+# physics were calculated -- whether falling or no. So just imitate the physics here only 
+# when actually falling.
+func _process(delta):
+	if _is_falling:
+		# Falling grav is less to give players a chance to react
+		position.y += gravity * delta * 0.09
 
 func _on_impact_area_body_entered(body):
-	if body is Player:
+	if body is Player and _is_falling:
 		body.knockout()
-	else:
-		# On freeze, re-snap x-axis
-		position.x = int(self.position.x) - (int(self.position.x) % 16) + 8
-		freeze = true
 
-func _on_falling():
-	freeze = false
+func _on_down_block_area_body_exited(body):
+	_is_falling = true
+	texture_sprite.visible = false
+
+func _on_down_block_area_body_entered(body):
+	if not body is Player:
+		_is_falling = false
+		_snap_to_grid()
+		texture_sprite.visible = true
---
+++ terrain/tile_map_block.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name TileMapBlock
+
+@export var name: String
+@export var is_mineable: bool = true
+@export var has_resource: bool = false
+@export var hitpoints: int = 100
